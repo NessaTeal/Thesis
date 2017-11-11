341e97d194319433ac22c002509e467fffccd348
Vadim Prokopev <vadimjprokopev@gmail.com>
2017-11-11 03:24:40 +0300

    Linear equations Python code

---
+++ Python/LinearEquations.py
@@ -0,0 +1,48 @@
+from copy import deepcopy
+
+def calc():
+    matrix = [[1,1,1],[0,2,5],[2,5,-1]]
+    vector = [6,-4,27]
+
+    determinant = getDeterminant(matrix)
+    adjugate = getAdjugate(matrix)
+    multiplied = multiplyMatrixAndVector(adjugate, vector)
+    answer = list(map(lambda x : x / determinant, multiplied))
+
+    print(answer)
+
+def getMinor(i, j, matrix):
+    matrix = deepcopy(matrix)
+
+    del matrix[i]
+
+    for x in range(len(matrix)):
+        del matrix[x][j]
+
+    return (-1) ** (i + j) * getDeterminant(matrix)
+
+def getDeterminant(matrix):
+    if len(matrix) == 2:
+        a = matrix[0][0]
+        b = matrix[0][1]
+        c = matrix[1][0]
+        d = matrix[1][1]
+        return a * d - b * c
+    else:
+        return sum(map(lambda y : matrix[0][y] * getMinor(0, y, matrix), range(len(matrix))))
+
+def getAdjugate(matrix):
+    adjugate = []
+
+    for y in range(len(matrix)):
+        adjugateRow = []
+        for x in range(len(matrix)):
+            adjugateRow.append(getMinor(x,y,matrix))
+        adjugate.append(adjugateRow)
+
+    return adjugate
+
+def multiplyMatrixAndVector(matrix, vector):
+    return list(map(lambda x : sum([(a * b) for (a,b) in zip(x, vector)]), matrix))
+
+calc()
